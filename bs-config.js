a0969fa19c3bfd07d9d5a92acf0c518225cbce8a
Cravcenco Nichita <nice_arti@mail.ru>
2022-01-13 21:08:50 +0200

    last

--- bs-config.js
+++ bs-config.js
@@ -2,8 +2,8 @@ module.exports =
 {
     port: process.env.PORT,
     files: ["./**/*.{html,htm,css,js}"],
-    server:
+    /*server:
     {
         baseDir: ["./src","./src/contracts"]
-    }
+    }*/
 }
