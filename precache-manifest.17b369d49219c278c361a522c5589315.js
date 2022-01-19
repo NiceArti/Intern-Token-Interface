cee343d2ed178bcbf52745c2f8ec8c77284c4891
Cravcenco Nichita <nice_arti@mail.ru>
2022-01-19 02:28:11 +0200

    Updates

---
+++ precache-manifest.17b369d49219c278c361a522c5589315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3769f00d5a713e0f68aa002b9fd2072",
+    "url": "/Intern-Token-Interface/index.html"
+  },
+  {
+    "revision": "f8a76e73cb80731de77d",
+    "url": "/Intern-Token-Interface/static/css/main.b3deb7e5.chunk.css"
+  },
+  {
+    "revision": "f12c992c90e0ec4cdd1b",
+    "url": "/Intern-Token-Interface/static/js/2.4b56409e.chunk.js"
+  },
+  {
+    "revision": "f8a76e73cb80731de77d",
+    "url": "/Intern-Token-Interface/static/js/main.6e0d4fe5.chunk.js"
+  },
+  {
+    "revision": "c909fbbcf76403e3b5f2",
+    "url": "/Intern-Token-Interface/static/js/runtime-main.26bb4312.js"
+  }
+]);
